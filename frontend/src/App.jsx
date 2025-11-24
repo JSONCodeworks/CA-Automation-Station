fd90ba697b5d1847403ee173f3b1148be3180662
JSON Adams <admin@jsoncodeworks.com>
2025-11-24 19:17:43 +0000

    Update frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import Layout from './components/Layout'
+import AdminLayout from './components/AdminLayout'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Resources from './pages/Resources'
-import Admin from './pages/Admin'
+import UserManagement from './pages/UserManagement'
+import MenuManagement from './pages/MenuManagement'
 import { useAuthStore } from './store/authStore'
 
 function App() {
@@ -27,7 +29,15 @@ function App() {
                 <Route path="/" element={<Navigate to="/dashboard" />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/resources/*" element={<Resources />} />
-                <Route path="/admin" element={<Admin />} />
+                <Route path="/admin/*" element={
+                  <AdminLayout>
+                    <Routes>
+                      <Route path="/" element={<Navigate to="/admin/users" />} />
+                      <Route path="/users" element={<UserManagement />} />
+                      <Route path="/menus" element={<MenuManagement />} />
+                    </Routes>
+                  </AdminLayout>
+                } />
               </Routes>
             </Layout>
           ) : (
